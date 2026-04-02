e2320f74912dd7840abe131e064fac72d7907269
Kriti Tyagi <tyagikriti0906@gmail.com>
2026-04-03 00:26:29 +0530

    Time: 3 ms (27.89%) | Memory: 97.4 MB (59.81%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minp=INT_MAX,maxp=0;
+        for(int price:prices){
+            minp=min(minp,price);
+            maxp=max(maxp,price-minp);
+        }
+        return maxp;
+    }
+};
