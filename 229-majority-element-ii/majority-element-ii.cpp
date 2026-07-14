13a5b0f17515aeaf9be721481e89c582b18b9766
Kriti Tyagi <tyagikriti0906@gmail.com>
2026-07-15 00:36:21 +0530

    Time: 10 ms (21.81%) | Memory: 26.2 MB (35.63%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res;
+        unordered_map<int,int>umap;
+        for(int i=0;i<n;i++)
+            umap[nums[i]]++;
+        for(auto it:umap){
+            if(it.second>n/3)
+                res.push_back(it.first);
+        }
+        return res;
+    }
+};
