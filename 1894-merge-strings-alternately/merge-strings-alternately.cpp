419f4e08405c16eac5b5249553000492f2f41b99
Kriti Tyagi <tyagikriti0906@gmail.com>
2026-08-31 14:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (30.16%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string str="";
+        int i=0,j=0;
+        while(i<word1.length() && j<word2.length()){
+            str+=word1[i++];
+            str+=word2[j++];
+        }
+        str+=word1.substr(i);
+        str+=word2.substr(j);
+        return str;
+    }
+};
